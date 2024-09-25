9b8805c2b05fea8af21058d22c50e5a4eafe55ee
Sourav Bhowal <souravbhowal15@gmail.com>
2024-09-26 00:49:42 +0530

    user pro

---
+++ src/components/user-profile/EditProfileButton.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { UserData } from "@/utils/types";
+import { useState } from "react";
+// import EditProfileDialog from "./EditProfileDialog";
+
+// EDIT PROFILE BUTTON PROPS
+interface EditProfileButtonProps {
+  user: UserData;
+}
+
+// EDIT PROFILE BUTTON COMPONENT
+export default function EditProfileButton({ user }: EditProfileButtonProps) {
+  // Dialog box state
+  const [showDialog, setShowDialog] = useState(false);
+
+  return (
+    <>
+      {/* EDIT PROFILE BUTTON */}
+      <Button variant={"outline"} onClick={() => setShowDialog(true)}>
+        Edit Profile
+      </Button>
+      {/* EDIT PROFILE DIALOG */}
+      {/* <EditProfileDialog
+        user={user}
+        open={showDialog}
+        onOpenChange={setShowDialog}
+      /> */}
+    </>
+  );
+}
